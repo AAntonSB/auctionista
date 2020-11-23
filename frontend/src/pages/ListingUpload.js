47f926c0d242110109555b5249e73041232b8f47
Daniela Bergman <daniela.bergman@hotmail.com>
2020-11-23 12:04:55 +0100

    added input fields in listing upload card

--- frontend/src/pages/ListingUpload.js
+++ frontend/src/pages/ListingUpload.js
@@ -1,51 +1,70 @@
 import { Card } from "@material-ui/core";
-import React from "react";
+import React, { useEffect } from "react";
 import { withRouter } from "react-router";
-import {Form, Row, Col, Label, Input, FormGroup, Button} from "reactstrap";
+import { Form, Row, Col, Label, Input, FormGroup } from "reactstrap";
 import "../stylings/Listings.css";
 
 const ListingUpload = (props) => {
+  const days = [];
+
+  useEffect(() => {
+    for (var i = 1; i <= 31; i++) {
+      days.push(i);
+    }
+    
+  }, []);
 
   return (
     <div id="listings-div">
       <Card id="listing-upload">
-        <Form>
-          <FormGroup row>
-            <Label for="exampleFile" sm={2}>
-              File
-            </Label>
-            <Col sm={10}>
-              <Input type="file" name="file" id="exampleFile" />
-            </Col>
-          </FormGroup>
-          <Row form>
+        <Form id="listing-form">
+          <Row>
             <Col md={6}>
               <FormGroup>
-                <Label for="exampleEmail">Email</Label>
-                <Input
-                  type="email"
-                  name="email"
-                  id="exampleEmail"
-                  placeholder="with a placeholder"
-                />
+                <Label for="item-image-upload">Upload Image</Label>
+                <Input type="file" name="file" id="item-image-upload" />
               </FormGroup>
             </Col>
             <Col md={6}>
               <FormGroup>
-                <Label for="examplePassword">Password</Label>
-                <Input
-                  type="password"
-                  name="password"
-                  id="examplePassword"
-                  placeholder="password placeholder"
-                />
+                <Label for="starting-price-input">Starting price</Label>
+                <Input id="starting-price-input" />
               </FormGroup>
             </Col>
           </Row>
+          <Row form>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="title-input">Title</Label>
+                <Input id="title-input" />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="reservation-price-input">Reservation price</Label>
+                <Input id="reservation-price-input" />
+              </FormGroup>
+            </Col>
+          </Row>
+          <FormGroup>
+            <Label for="description-text">Item Description</Label>
+            <Input type="textarea" name="text" id="description-text" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="end-time-select">Choose end time</Label>
+            <Input type="select" name="select" id="end-time-select">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+            </Input>
+          </FormGroup>
         </Form>
       </Card>
     </div>
   );
-
-}
+};
 export default withRouter(ListingUpload);
