80716c998fdb2cd8a8f20365f42dc1f2ab40907c
AAntonSB <anton.s.bergqvist@gmail.com>
2020-11-28 15:51:13 +0100

    thumbnail html  & css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,15 @@ import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
-import MessageContext from "./contexts/MessageContextProvider";
-import PrintMessages from "./components/PrintMessages";
+import listingThumbnail from "./components/ListingThumbnail";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <MessageContext>
-          <Switch>
-            <Route exact path="/" component={PrintMessages} />
-          </Switch>
-        </MessageContext>
+        <Switch>
+          <Route exact path="/" component={listingThumbnail} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
