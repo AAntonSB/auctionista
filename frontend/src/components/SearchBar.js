30215e7763623f745436c1120bcd3a1d74e95621
Daniela Bergman <daniela.bergman@hotmail.com>
2020-12-01 10:09:51 +0100

    Styling

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react"; 
 import { withRouter } from "react-router"
-import { FormGroup, Label, Col, Input } from "reactstrap";
+import { FormGroup, Label, Col, Input, Button } from "reactstrap";
 import "../stylings/Listings.css";
 
 const SearchBar = (props) => {
@@ -9,15 +9,15 @@ const SearchBar = (props) => {
   return (
     <div id="search-bar-div">
       <FormGroup row id="search-bar-form">
-        <Label for="search-bar-input" sm={2}>
-          Search
-        </Label>
-        <Col sm={10}>
+        <Col sm={7}>
           <Input
             type="search"
             id="search-bar-input"
-            placeholder="with a placeholder"
+            placeholder="Search"
           />
+        </Col>
+        <Col sm={1}>
+          <Button id="search-button">Search</Button>
         </Col>
       </FormGroup>
     </div>
