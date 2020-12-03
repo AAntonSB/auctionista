ffcbd1b14144938b08b28a26ed24352c510b4c17
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-03 17:04:04 +0100

    about us page

---
+++ frontend/src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../css/About.css'
+
+const About = () => {
+  return (
+    <div class="parent white">
+      <div class="span-12 section">
+        <p className="bold-text">About us</p>
+      </div>
+      <div class="span-6 section">
+        <p className="small-text">We are the first nordic super interactive interconected super marketplace for auctions. </p>
+      </div>
+      <div class="span-4 section">
+        <p className="small-text">We on our team of super duper mega professionals are currently working on the slickest super solutions for all auctions.</p>
+      </div>
+      <div class="span-2 section">
+        <p className="small-text">For work contact info@Theauctionista.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
