0b4145612739ee4338c82512fdbaf307cab14f17
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-04 13:31:31 +0100

    Fleshed out listing-details

--- frontend/src/components/ListingDetails.js
+++ frontend/src/components/ListingDetails.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useContext, useState } from "react";
 import { withRouter } from "react-router";
-import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+import {} from "reactstrap";
 import { ListingContext } from "../contexts/ListingContextProvider";
-import "../css/Listings.css";
+import "../css/ListingDetails.css";
 
 const ListingDetails = (props) => {
   const listingContext = useContext(ListingContext);
@@ -32,18 +32,74 @@ const ListingDetails = (props) => {
     }
   }, [listing]);
 
+  const DisplayEndTime = () => {
+    let uni = new Date(listing.endDate * 1000);
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    let month = months[uni.getMonth()];
+    let date = uni.getDate();
+    var hour = uni.getHours();
+    var min = uni.getMinutes();
+    return (
+      <>
+        {date} {month} {hour.length < 2 ? hour : "0" + hour + ":" + min}
+      </>
+    );
+  };
+
   return (
-    <div className="listings-div">
-      <Card id="listing-details">
-        <CardImg top width="100%" src={displayImage} alt="Card image cap" />
-        <CardBody id="details-text">
-          <CardTitle tag="h5">{listing.title}</CardTitle>
-          <CardText>
-            <small className="text-muted">{listing.description}</small>
-          </CardText>
-        </CardBody>
-      </Card>
-      <image src={"../logo.svg"}/>
+    <div className="item-details-body">
+      <div className="details-block">
+        <h1 className="title-font">{listing.title}</h1>
+        <div className="image-showcase">
+          <main class="section coral" contenteditable>
+            <img className="display-img" src={displayImage}></img>
+          </main>
+          <footer className="nav-text">
+            <div class="circle">
+              <div class="arrow"></div>
+            </div>
+            <div class="circle rotate-right right-align">
+              <div class="arrow"></div>
+            </div>
+          </footer>
+        </div>
+        <div className="item-detail-description">
+          <p>{listing.description}</p>
+        </div>
+      </div>
+      <div className="payment-block">
+        <div className="starting-price">
+          <p className="payment-cursive-text center-text">Starting price</p>
+          <p className="payment-regular-text center-text">
+            {listing.startingBid} kr
+          </p>
+        </div>
+        <div className="end-time">
+          <p className="payment-cursive-text center-text">
+            {" "}
+            Ends <DisplayEndTime />
+          </p>
+          <p className="payment-regular-text">Time to end</p>
+        </div>
+        <div className="bids">
+          <p className="payment-cursive-text center-text">Bids</p>
+          <p className="payment-regular-text">bids {"0"}</p>
+        </div>
+        <div className="payment-component">Place payment component here</div>
+      </div>
     </div>
   );
 };
