5612c0cb89e4e20a3fc701944fab9fcd2d6bb2e5
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-07 11:10:33 +0100

    installed scheduler

--- src/main/java/com/grupp4/auctionista/AuctionistaApplication.java
+++ src/main/java/com/grupp4/auctionista/AuctionistaApplication.java
@@ -2,11 +2,14 @@ package com.grupp4.auctionista;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+@EnableScheduling
 public class AuctionistaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AuctionistaApplication.class, args);
 	}
 
