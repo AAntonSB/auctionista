ef38bed72d701dc61969a4849a5eeb3161aa2140
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-07 11:56:31 +0100

    close down button

--- frontend/src/components/RegisterModal.js
+++ frontend/src/components/RegisterModal.js
@@ -4,76 +4,69 @@ import { Modal } from "reactstrap";
 
 const LoginModal = () => {
   return (
-      <Modal isOpen={true} className="ModalMoves" autoFocus={false}>
-        <form>
-          <h1>Register</h1>
-          <section>
-            <label for="email" className="block-label">
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              autocomplete="email"
-              autoFocus
-              required
-              className="inputFit"
-            />
-          </section>
+    <Modal isOpen={true} className="ModalMoves" autoFocus={false}>
+      <button className="close-button topright">x</button>
+      <form>
+        <h1>Register</h1>
+        <section>
+          <label for="email" className="block-label">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            autocomplete="email"
+            autoFocus
+            required
+            className="inputFit"
+          />
+        </section>
 
-          <section>
-            <label for="username" className="block-label">
-              User name
-            </label>
-            <input
-              id="username"
-              name="username"
-              required
-              className="inputFit"
-            />
-          </section>
+        <section>
+          <label for="username" className="block-label">
+            User name
+          </label>
+          <input id="username" name="username" required className="inputFit" />
+        </section>
 
-          <section>
-            <label for="password" className="block-label" type="password">
-              Password
-            </label>
-            <div className="passwordBox">
-              <input
-                id="password"
-                type="password"
-                name="new-password"
-                autocomplete="new-password"
-                aria-describedby="password-constraints"
-                required
-              />
-            </div>
-            <div
-              id="password-constraints"
-              className="password-constraints-font"
-            >
-              8+ Characters with a mix of letters, numbers and symbols
-            </div>
-          </section>
-
-          <section>
-            <label for="confirmPassword" className="block-label">
-              Confirm password
-            </label>
+        <section>
+          <label for="password" className="block-label" type="password">
+            Password
+          </label>
+          <div className="passwordBox">
             <input
-              id="confirmPassword"
+              id="password"
               type="password"
               name="new-password"
               autocomplete="new-password"
+              aria-describedby="password-constraints"
               required
-              className="inputFit"
             />
-          </section>
+          </div>
+          <div id="password-constraints" className="password-constraints-font">
+            8+ Characters with a mix of letters, numbers and symbols
+          </div>
+        </section>
 
-          <button>Create account</button>
-          <p className="login-route">Already have an account? click me! </p>
-        </form>
-      </Modal>
+        <section>
+          <label for="confirmPassword" className="block-label">
+            Confirm password
+          </label>
+          <input
+            id="confirmPassword"
+            type="password"
+            name="new-password"
+            autocomplete="new-password"
+            required
+            className="inputFit"
+          />
+        </section>
+
+        <button>Create account</button>
+        <p className="login-route">Already have an account? click me! </p>
+      </form>
+    </Modal>
   );
 };
 
