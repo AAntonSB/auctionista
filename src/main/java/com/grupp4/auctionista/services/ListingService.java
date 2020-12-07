cb7a07c16ac67afa864eff7dc9b26d062c9646a2
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-07 10:37:31 +0100

    imports

--- src/main/java/com/grupp4/auctionista/services/ListingService.java
+++ src/main/java/com/grupp4/auctionista/services/ListingService.java
@@ -1,13 +1,12 @@
 package com.grupp4.auctionista.services;
 
 import com.grupp4.auctionista.entities.Listing;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.grupp4.auctionista.repositories.ListingRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
