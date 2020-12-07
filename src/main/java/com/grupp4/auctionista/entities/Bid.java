6b9e236fa54f773edb70bb56da9b64e31a9a6f9a
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-07 11:11:22 +0100

    dummy bid entity

---
+++ src/main/java/com/grupp4/auctionista/entities/Bid.java
@@ -0,0 +1,24 @@
+package com.grupp4.auctionista.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Table(name = "bid")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bid {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private long amount;
+    private long timestamp;
+}
