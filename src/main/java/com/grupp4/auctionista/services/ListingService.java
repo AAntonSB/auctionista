869e750766d1ef19116a34f8966bd089e3814a3b
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-07 11:14:28 +0100

    Created service functions for maintaining active listings

--- src/main/java/com/grupp4/auctionista/services/ListingService.java
+++ src/main/java/com/grupp4/auctionista/services/ListingService.java
@@ -1,27 +1,38 @@
 package com.grupp4.auctionista.services;
 
+import com.grupp4.auctionista.entities.Bid;
+import com.grupp4.auctionista.entities.ActiveListing;
+import com.grupp4.auctionista.entities.ActiveListings;
 import com.grupp4.auctionista.entities.Listing;
+import com.grupp4.auctionista.repositories.BidRepo;
 import com.grupp4.auctionista.repositories.ListingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.annotation.PostConstruct;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Service
 public class ListingService {
+    private final ConcurrentMap<UUID, Object> locks = new ConcurrentHashMap<UUID, Object>();
     @Autowired
     ListingRepo listingRepo;
-
+    @Autowired
+    BidRepo bidRepo;
 
     public List<Listing> getAllListings() {
         return listingRepo.findAll();
     }
 
-    public Listing getListingById(UUID id){
+    public Listing getListingById(UUID id) {
         return listingRepo.findById(id).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not find the listing"));
     }
@@ -33,7 +44,7 @@ public class ListingService {
     public void deleteListing(UUID id) {
         listingRepo.deleteById(id);
     }
-    
+
     public Optional<Listing> getById(UUID id) {
         return listingRepo.findById(id);
     }
@@ -44,5 +55,30 @@ public class ListingService {
 
         System.out.println(listings);
         return listings;
+    }
+}
+
+    @PostConstruct
+    public void initFlups() {
+        var listings = getAllListings();
+        var activeListings = ActiveListings.getInstance();
+        System.out.println("hej nu startar vi");
+        activeListings.setActiveListings(listings.stream()
+                .map((listing) -> new ActiveListing(listing.getId(), listing.getEndDate()))
+                .sorted(Comparator.comparingLong(ActiveListing::getTimestamp)).toArray(ActiveListing[]::new));
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void checkActiveAuctions() {
+        var activeListings = ActiveListings.getInstance();
+
+        long now = TimeStampService.getTimestamp();
+        System.out.println("hej nu kör vi");
+
+        var expiredListings = activeListings.expireListingsFrom(now);
+        for (UUID expiredListingId : expiredListings) {
+            var listing = getListingById(expiredListingId);
+            // make the listings highest bidder the winner
+        }
     }
 }
