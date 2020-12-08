f96177d337f484074b2498b8935478b52d31bcbd
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-08 11:00:45 +0100

    notification provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,27 +12,30 @@ import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import ListingList from "./components/ListingList";
 import About from "./components/About";
+import NotificationProvider from "./providers/NotificationProvider";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <ListingContext>
-          <MessageContext>
-            <Header/>
-            <Switch>
-          <Route exact path="/" component={ListingList} />
-              <Route exact path="/upload-listing" component={ListingUpload} />
-              <Route
-                exact
-                path="/listing-details/:id"
-                component={ListingDetails}
-              />
-              <Route exact path="/thumbnail" component={ListingThumbnail} />
-              <Route exact path="/about" component={About}/>
-            </Switch>
-          </MessageContext>
-        </ListingContext>
+        <NotificationProvider>
+          <ListingContext>
+            <MessageContext>
+              <Header />
+              <Switch>
+                <Route exact path="/" component={ListingList} />
+                <Route exact path="/upload-listing" component={ListingUpload} />
+                <Route
+                  exact
+                  path="/listing-details/:id"
+                  component={ListingDetails}
+                />
+                <Route exact path="/thumbnail" component={ListingThumbnail} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </MessageContext>
+          </ListingContext>
+        </NotificationProvider>
       </div>
     </BrowserRouter>
   );
