924dd101a4e0bc45d7bafb40e3c4785e92a3c9f4
Daniela Bergman <daniela.bergman@hotmail.com>
2020-12-08 17:36:47 +0100

    Added input field for placing bids

--- frontend/src/components/Bidding.js
+++ frontend/src/components/Bidding.js
@@ -9,22 +9,24 @@ const Bidding = (props) => {
     <div className="payment-block">
       <div className="starting-price">
         <p className="payment-cursive-text">Highest bid</p>
-        <p className="payment-regular-text">
-          {props.startingBid} kr
-        </p>
+        <p className="payment-regular-text">{props.startingBid} kr</p>
       </div>
       <div className="end-time">
-        <p className="payment-cursive-text">
-          {" "}
-          Ends
-        </p>
+        <p className="payment-cursive-text"> Ends</p>
         <TimeLeft {...props} />
       </div>
       <div className="bids">
         <p className="payment-cursive-text">Bids</p>
         <p className="payment-regular-text">bids {"0"}</p>
       </div>
-      <div className="payment-component">Place payment component here</div>
+      <div className="payment-component">
+        <div className="payment-box">
+          <label for="bidding-input" id="bidding-label">
+            Place bid
+          </label>
+          <input type="text" id="bidding-input" placeholder="kr"></input>
+        </div>
+      </div>
     </div>
   );
 }
