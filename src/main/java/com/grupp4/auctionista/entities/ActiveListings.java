5c15940f2f21efa892c2b3763ce35144a7e1473c
AAntonSB <anton.s.bergqvist@gmail.com>
2020-12-12 17:20:19 +0100

    Fixed adding to array when program is running
    Joakim & Anton

--- src/main/java/com/grupp4/auctionista/entities/ActiveListings.java
+++ src/main/java/com/grupp4/auctionista/entities/ActiveListings.java
@@ -24,7 +24,7 @@ public class ActiveListings {
         ArrayList<UUID> result = new ArrayList<>();
 
         for (int i = 0; i < activeListings.length; i++) {
-            if (activeListings[i].getTimestamp() < now) {
+            if (activeListings[i].getTimestamp() <= now) {
                 result.add(activeListings[i].getId());
             } else {
                 System.arraycopy(activeListings, i, activeListings, 0, activeListings.length - i - 1);
@@ -32,5 +32,17 @@ public class ActiveListings {
             }
         }
         return result;
+    }
+
+    public void addToActiveListingToListing(ActiveListing activeListing){
+        activeListings = addTo(activeListings, activeListing);
+    }
+
+    private ActiveListing[] addTo(ActiveListing[] arr, ActiveListing... elements){
+        ActiveListing[] tempArr = new ActiveListing[arr.length+elements.length];
+        System.arraycopy(arr, 0, tempArr, 0, arr.length);
+        System.arraycopy(elements, 0, tempArr, arr.length, elements.length);
+        return tempArr;
+
     }
 }
